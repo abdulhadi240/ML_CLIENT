851fbb900ec363ea6c1d1f503a7f3240aaa7d163
abdulhadi240 <ah912425@gmail.com>
2024-04-27 00:27:19 +0500

    shadcn added

--- src/app/input/page.jsx
+++ src/app/input/page.jsx
@@ -1,14 +1,23 @@
-'use client'
+"use client";
 import React, { useState } from "react";
 import Papa from "papaparse";
 import { useRouter } from "next/navigation"; // Import `useRouter` from 'next/router'
-import useUploadStore from "@/utils/state"
+import useUploadStore from "@/utils/state";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const Page = () => {
   const [disable, setDisable] = useState(true);
+  const [data , setData] = useState(null)
   const router = useRouter(); // Use `useRouter` hook to access the router object
   const setParsedData = useUploadStore((state) => state.setParsedData);
-
 
   const handleFile = (e) => {
     const csv_file = e.target.files[0];
@@ -17,16 +26,21 @@ const Page = () => {
         console.log(results);
         setDisable(false);
         setParsedData(results);
+        setData(results.data)
       },
     });
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen mx-4 sm:mx-0">
       <div className="mb-3 w-96">
-        <label htmlFor="formFile" className="inline-block mb-2 text-gray-700 form-label"></label>
+        <label
+          htmlFor="formFile"
+          className="inline-block mb-2 text-gray-700 form-label"
+        ></label>
         <input
-          className="form-control
+          className={`form-control ${disable ? "-mt-0" : "-mt-44"}
+          
             block
             w-full
             px-3
@@ -35,31 +49,51 @@ const Page = () => {
             font-normal
             text-gray-700
             bg-white bg-clip-padding
-            border border-solid border-gray-300
+            border border-dashed border-gray-300
             rounded
             transition
             ease-in-out
             m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none`}
           type="file"
           id="formFile"
           accept=".csv"
           onChange={handleFile} // Corrected function name to `handleFile`
         />
-        <div className="items-center mt-5 text-center justfiy-center ">
+        <div className="items-center justify-center mt-5 text-center"> {/* Corrected typo in class name */}
           {disable ? (
-            <button className="border-[1px] p-3 disabled:cursor-not-allowed rounded-md bg-black text-white" disabled>
+            <button
+              className="border-[1px] p-3 disabled:cursor-not-allowed rounded-md bg-black text-white"
+              disabled
+            >
               Choose the File
             </button>
           ) : (
-            <button
-              className="border-[1px] p-3 disabled:cursor-not-allowed rounded-md bg-black text-white"
-              onClick={() => {
-                router.push("/dashboard");
-              }}
-            >
-              Continue
-            </button>
+            <div>
+              <button
+                className="border-[1px] p-3 disabled:cursor-not-allowed rounded-md bg-black text-white"
+                onClick={() => {
+                  router.push("/dashboard");
+                }}
+              >
+                Continue
+              </button>
+              <Table>
+                <TableBody>
+                  {data && data.map((items,index) => (
+                    <TableRow key={index}>
+                      <TableCell className="font-medium">{items[0]}</TableCell>
+                      <TableCell>{items[1]}</TableCell>
+                      <TableCell>{items[2]}</TableCell>
+                      <TableCell>{items[3]}</TableCell>
+                      <TableCell>{items[4]}</TableCell>
+                      <TableCell>{items[5]}</TableCell>
+                      <TableCell className="text-right">{items[6]}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
           )}
         </div>
       </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { cn } from "@/lib/utils"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+        )}>{children}</body>
     </html>
   );
 }
